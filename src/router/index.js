6b801bf215641bfe68350d4029c28e85f722dc6c
Dayna Clare <daycanimate@gmail.com>
2024-04-27 01:56:22 -0600

    Updated profile view

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import PostCard from '../components/PostCard.vue'
-import ProfileView from '../views/ProfileView.vue'
+import ProfileView from '../components/ProfileView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
